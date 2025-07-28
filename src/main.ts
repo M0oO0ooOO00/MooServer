b0fced41e18920d363d7f484e74bdb4fc791d2e0
chyun7114 <chyun7114@naver.com>
2025-07-28 21:47:50 +0900

    Feat: Swagger 설정 완료

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { SwaggerConfig } from './common/config/swagger.config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
 
     // HttpException filter
     app.useGlobalFilters(new HttpExceptionFilter());
+
+    // swagger-config
+    SwaggerConfig.setUp(app);
 
     await app.listen(process.env.PORT ?? 3030);
 }
