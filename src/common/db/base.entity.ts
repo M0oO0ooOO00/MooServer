8bd4d559b8baa198c7f95e062550f3cd5807e6f2
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-05 23:17:30 +0900

    feat: 업데이트 시 자동으로 updated_at을 현재 시간으로 변경시키기

--- src/common/db/base.entity.ts
+++ src/common/db/base.entity.ts
@@ -4,6 +4,6 @@ import { sql } from 'drizzle-orm';
 export const baseColumns = {
     id: serial('id').primaryKey(),
     createdAt: timestamp('created_at', { mode: 'date' }).default(sql`now()`),
-    updatedAt: timestamp('updated_at', { mode: 'date' }).default(sql`now()`),
+    updatedAt: timestamp('updated_at', { mode: 'date' }).default(sql`now()`).$onUpdate(() => sql`now()`),
     deleted: boolean('deleted').default(false),
 };
