b8b662a86b792e73f36bd1c05e97bc1b37bedfcb
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-19 16:12:18 +0900

    feat: member 엔티티 및 Role 이넘 생성

--- src/member/domain/member.entity.ts
+++ src/member/domain/member.entity.ts
@@ -1,8 +1,23 @@
-import { pgTable, text } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, text } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 import { baseColumns } from '../../common/db/base.entity';
+import { Role } from '../../common/enums/role.enum';
+import { Profile } from './profile.entity';
+
+export const roleEnum = pgEnum(
+    'role',
+    Object.values(Role) as [string, ...string[]],
+);
 
 export const Member = pgTable('member', {
     ...baseColumns,
     name: text('name').notNull(),
     email: text('email').notNull(),
+    birthDate: text('birth_date').notNull(),
+    phoneNumber: text('phone_number'),
+    role: roleEnum('role').notNull(),
 });
+
+export const memberRelations = relations(Member, ({ one }) => ({
+    Profile: one(Profile),
+}));
