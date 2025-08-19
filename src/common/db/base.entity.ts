74d3070ae1494cc528b84137a4c93ac6933b64cd
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-19 16:11:50 +0900

    feat: pnpm-lock 타입 수정 및 formatting

--- src/common/db/base.entity.ts
+++ src/common/db/base.entity.ts
@@ -4,6 +4,8 @@ import { sql } from 'drizzle-orm';
 export const baseColumns = {
     id: serial('id').primaryKey(),
     createdAt: timestamp('created_at', { mode: 'date' }).default(sql`now()`),
-    updatedAt: timestamp('updated_at', { mode: 'date' }).default(sql`now()`).$onUpdate(() => sql`now()`),
+    updatedAt: timestamp('updated_at', { mode: 'date' })
+        .default(sql`now()`)
+        .$onUpdate(() => sql`now()`),
     deleted: boolean('deleted').default(false),
 };
