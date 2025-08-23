4093294524fb06000fe84ed396799c642807f349
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-23 22:42:10 +0900

    feat: app.module에 DI를 위한 import 추가

--- src/common/db/schema/index.ts
+++ src/common/db/schema/index.ts
@@ -1,7 +1,10 @@
 // nestjs ORM 세팅을 위한 파일
 // entity를 만들고 해당 파일에 export해야 작동함
 
-export * from '../../../member/domain/member.entity';
-export * from '../../../member/domain/profile.entity';
-export * from '../../../report/domain/report.entity';
-export * from '../../../report/domain/report-count.entity';
+export * from '../../../member/domain';
+export * from '../../../report/domain';
+export * from '../../../post/domain';
+export * from '../../../comment/domain';
+export * from '../../../participation/domain';
+export * from '../../../scrap/domain';
+export * from '../enums';
