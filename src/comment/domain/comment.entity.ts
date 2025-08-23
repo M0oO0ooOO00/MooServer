626f682cc28a1a68af9f25744a1bb4e9a93440f9
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-23 22:40:53 +0900

    feat: 댓글 도메인 구현

---
+++ src/comment/domain/comment.entity.ts
@@ -0,0 +1,30 @@
+import { integer, pgTable, text } from 'drizzle-orm/pg-core';
+import { baseColumns } from '../../common/db/base.entity';
+import { StatusPgEnum } from '../../common/db/enums';
+import { Member } from '../../member/domain';
+import { Post } from '../../post/domain';
+import { relations } from 'drizzle-orm';
+
+export { StatusPgEnum };
+export const Comment = pgTable('comment', {
+    ...baseColumns,
+    content: text('content').notNull(),
+    status: StatusPgEnum('status').notNull(),
+    authorId: integer('author_id')
+        .notNull()
+        .references(() => Member.id),
+    postId: integer('post_id')
+        .notNull()
+        .references(() => Post.id),
+});
+
+export const commentRelations = relations(Comment, ({ one }) => ({
+    author: one(Member, {
+        fields: [Comment.authorId],
+        references: [Member.id],
+    }),
+    post: one(Post, {
+        fields: [Comment.postId],
+        references: [Post.id],
+    }),
+}));
