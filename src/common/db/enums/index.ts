4eee90af627bdd5af128042b76798b3974ce140a
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-23 23:14:58 +0900

    feat: eslint format

--- src/common/db/enums/index.ts
+++ src/common/db/enums/index.ts
@@ -1,4 +1,4 @@
-export * from './recruitement-role.pg-enum';
+export * from './recruitment-role.pg-enum';
 export * from './status.pg-enum';
 export * from './post-type.pg-enum';
 export * from './role.pg-enum';
