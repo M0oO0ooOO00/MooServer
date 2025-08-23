00fbc085f9e7eaff6deeacd74d4f84504f2d2a39
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-23 22:41:13 +0900

    feat: 멤버 도메인에 각 테이블 간 관계 추가

--- src/member/domain/member.entity.ts
+++ src/member/domain/member.entity.ts
@@ -1,15 +1,15 @@
-import { pgEnum, pgTable, text } from 'drizzle-orm/pg-core';
+import {pgTable, text } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
 import { baseColumns } from '../../common/db/base.entity';
-import { Role } from '../../common/enums/role.enum';
 import { Profile } from './profile.entity';
-import { ReportCount } from '../../report/domain/report-count.entity';
-import { Report } from '../../report/domain/report.entity';
+import { ReportCount } from '../../report/domain';
+import { Report } from '../../report/domain';
+import { Post } from '../../post/domain';
+import { Participation } from '../../participation/domain';
+import { Scrap } from '../../scrap/domain';
+import { rolePgEnum } from '../../common/db/enums/role.pg-enum';
 
-export const roleEnum = pgEnum(
-    'role',
-    Object.values(Role) as [string, ...string[]],
-);
+export { rolePgEnum };
 
 export const Member = pgTable('member', {
     ...baseColumns,
@@ -17,11 +17,14 @@ export const Member = pgTable('member', {
     email: text('email').notNull(),
     birthDate: text('birth_date').notNull(),
     phoneNumber: text('phone_number'),
-    role: roleEnum('role').notNull(),
+    role: rolePgEnum('role').notNull(),
 });
 
-export const memberRelations = relations(Member, ({ one }) => ({
+export const memberRelations = relations(Member, ({ one, many }) => ({
     profile: one(Profile),
+    post: many(Post),
     reportCount: one(ReportCount),
     report: one(Report),
+    participation: many(Participation),
+    scrap: many(Scrap),
 }));
