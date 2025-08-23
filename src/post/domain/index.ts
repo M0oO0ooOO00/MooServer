a14ca8a7e60db6ef1fd2e1f41772c253bbbce2ef
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-23 22:39:36 +0900

    feat: 게시글 관련 엔티티 구현 - 직관꿀팁 - 직관메이트 모집 - 확장성을 고려해서 부모역할을 할 Post 엔티티를 생성하고, 직관꿀팁, 직관메이트와 각각 1:1 관계를 맺음

---
+++ src/post/domain/index.ts
@@ -0,0 +1,4 @@
+export * from './post.entity';
+export * from './recruitment-detail.entity';
+export * from './tips-detail.entity';
+export * from './post-image.entity';
--- src/post/domain/post.entity.ts
+++ src/post/domain/post.entity.ts
@@ -1,3 +1,33 @@
-import { pgTable } from 'drizzle-orm/pg-core';
+import { integer, pgTable, text } from 'drizzle-orm/pg-core';
+import { baseColumns } from '../../common/db/base.entity';
+import { StatusPgEnum } from '../../common/db/enums';
+import { Member } from '../../member/domain';
+import { relations } from 'drizzle-orm';
+import { TipsDetail } from './tips-detail.entity';
+import { RecruitmentDetail } from './recruitment-detail.entity';
+import { PostImage } from './post-image.entity';
+import { Scrap } from '../../scrap/domain';
+import { postTypePgEnum } from '../../common/db/enums/post-type.pg-enum';
 
-export const Post = pgTable
+export { StatusPgEnum, postTypePgEnum };
+
+export const Post = pgTable('post', {
+    ...baseColumns,
+    title: text('title').notNull(),
+    post_type: postTypePgEnum('post_type').notNull(),
+    status: StatusPgEnum('status').notNull(),
+    authorId: integer('author_id')
+        .notNull()
+        .references(() => Member.id),
+});
+
+export const postRelations = relations(Post, ({ one, many }) => ({
+    author: one(Member, {
+        fields: [Post.authorId],
+        references: [Member.id],
+    }),
+    recruitmentDetail: one(RecruitmentDetail),
+    tipsDetail: one(TipsDetail),
+    postImage: many(PostImage),
+    scrap: many(Scrap),
+}));
