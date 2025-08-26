be7b5e4c5be19b9132ef6cd693aed68344db8fc3
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-26 15:31:24 +0900

    feat: domain.exception.ts 에 에러 발생 url 추가

--- src/common/exceptions/domain.exception.ts
+++ src/common/exceptions/domain.exception.ts
@@ -2,20 +2,19 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class DomainException extends HttpException {
     constructor(
-        statusCode: number,
+        code: number,
         message: string | Record<string, any>,
         public readonly errorCode?: string,
         public readonly path?: string,
     ) {
         super(
             {
-                statusCode,
-                errorCode: errorCode ?? HttpStatus[statusCode],
+                errorCode: errorCode ?? HttpStatus[code],
                 message,
                 path: path ?? null,
                 timestamp: new Date().toISOString(),
             },
-            statusCode,
+            code,
         );
         Error.captureStackTrace(this, new.target); // BaseException의 생성자 호출은 스택 트레이스에서 제외
     }
