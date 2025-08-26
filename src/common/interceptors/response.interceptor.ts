a8c7f47f167a5e679a72486c8ae6ded89de68103
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-26 13:23:55 +0900

    feat: 응답 및 에러 형식 통일

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -5,21 +5,41 @@ import {
     NestInterceptor,
 } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
+import { Request, Response } from 'express';
+import { CommonResponse } from '../response/common.response';
 
 @Injectable()
-export class ResponseInterceptor<T> implements NestInterceptor<T, any> {
+export class ResponseInterceptor implements NestInterceptor {
     intercept(
         context: ExecutionContext,
-        next: CallHandler<T>,
+        next: CallHandler<any>,
     ): Observable<any> | Promise<Observable<any>> {
-        return next.handle().pipe(
-            map((data) => {
-                return {
-                    statusCode: context.switchToHttp().getResponse().statusCode,
-                    message: '요청이 성공적으로 처리되었습니다.',
-                    data: data ?? null,
-                };
-            }),
-        );
+        const transformResponse = (data: unknown) => {
+            const req = context.switchToHttp().getRequest<Request>();
+            const res = context.switchToHttp().getResponse<Response>();
+
+            const { statusCode } = this.parseMetadatas(req, res);
+
+            return new CommonResponse(statusCode, data);
+        };
+
+        return next.handle().pipe(map(transformResponse));
     }
+
+    private parseMetadatas = function (req: Request, res: Response) {
+        const statusCode = res.statusCode;
+
+        const user = req?.user;
+        const ip = req?.ip;
+        const method = req?.method;
+        const originalUrl = req?.originalUrl;
+
+        return {
+            statusCode,
+            user,
+            ip,
+            method,
+            originalUrl,
+        };
+    };
 }
