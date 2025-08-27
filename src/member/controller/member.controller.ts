8b134df1dba9cbdfd7b572ddadcbce00d627680f
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-27 13:56:49 +0900

    feat: MemberController 에 스웨거 적용

--- src/member/controller/member.controller.ts
+++ src/member/controller/member.controller.ts
@@ -1,24 +1,34 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { MemberService } from '../service/member.service';
-import { CreateMemberDto } from '../dto/create-member.dto';
+import { CreateMemberRequest } from '../dto/request/create-member.request';
+import {
+    CreateMemberSwagger,
+    GetMembersSwagger,
+    GetMemberSwagger,
+    MemberControllerSwagger,
+} from '../swagger';
 
 @Controller('member')
+@MemberControllerSwagger
 export class MemberController {
     constructor(private readonly memberService: MemberService) {}
 
     @Post()
-    async signUp(@Body() createMemberDto: CreateMemberDto) {
+    @CreateMemberSwagger
+    async signUp(@Body() createMemberDto: CreateMemberRequest) {
         const { name, email } = createMemberDto;
         // TODO : 바뀐 엔티티에 따라 다시 만들어야 함.
         await this.memberService.createMember(name, email);
     }
 
     @Get('all')
+    @GetMembersSwagger
     async getAllMembers() {
         return await this.memberService.findAll();
     }
 
     @Get(':id')
+    @GetMemberSwagger
     async getMemberById(@Param('id') id: number) {
         return await this.memberService.findOneById(id);
     }
