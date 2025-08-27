6d17ffc1a9ff3d77ce970efe08447102ba26bef1
HoyeongJeon <hoyeong0302@gmail.com>
2025-08-27 13:55:17 +0900

    feat: 멤버 리스트 검색 dto 및 스웨거 문서 작성

--- src/member/dto/response/get-members.response.ts
+++ src/member/dto/response/get-members.response.ts
@@ -1,46 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { GetMemberResponse } from './get-member.response';
 
-export class MemberSummaryDto {
-    @ApiProperty({
-        description: '사용자 ID',
-        type: 'integer',
-    })
-    id: number;
+export class GetMembersResponse {
+    @ApiProperty({ type: [GetMemberResponse] })
+    members: GetMemberResponse[];
 
-    @ApiProperty({
-        description: '사용자 이름',
-        type: 'string',
-    })
-    name: string;
-
-    @ApiProperty({
-        description: '생성일',
-        type: 'string',
-        format: 'date-time',
-    })
-    createdAt: Date;
-
-    static from(member: any): MemberSummaryDto {
-        const dto = new MemberSummaryDto();
-        dto.id = member.id;
-        dto.name = member.name;
-        dto.createdAt = member.createdAt;
-        return dto;
-    }
-}
-
-export class GetMembersResponseDto {
-    @ApiProperty({ type: [MemberSummaryDto] })
-    members: MemberSummaryDto[];
-
-    constructor(members: MemberSummaryDto[]) {
+    constructor(members: GetMemberResponse[]) {
         this.members = members;
     }
 
-    static from(members: any[]): GetMembersResponseDto {
-        const memberSummaries = members.map((member) =>
-            MemberSummaryDto.from(member),
+    static from(members: any[]): GetMembersResponse {
+        const memberSummary = members.map((member) =>
+            GetMemberResponse.from(member),
         );
-        return new GetMembersResponseDto(memberSummaries);
+        return new GetMembersResponse(memberSummary);
     }
 }
