d11a922ccb88af8d68474f67b7e52c9f2f22a77f
HoyeongJeon <hoyeong0302@gmail.com>
2025-09-12 09:47:23 +0900

    feat: Report 모듈 Repository 패턴 적용
    - ReportRepository 클래스 구현
    - 멤버별 신고 내역 조회 기능
    - ReportModule Provider 등록

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ReportRepository } from './repository';
+import { DbModule } from '../common/db/db.module';
 
-@Module({})
+@Module({
+    imports: [DbModule],
+    providers: [ReportRepository],
+    exports: [ReportRepository],
+})
 export class ReportModule {}
