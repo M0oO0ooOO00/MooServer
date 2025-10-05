dffa6350e833a0c7253354543cea155371d34c18
Choi Yong Won <cywin0821@gmail.com>
2025-10-05 16:33:06 +0900

    feat: socket.io를 이용하여 코드를 수정

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,18 +1,28 @@
-// src/chat/chat.gateway.ts
-
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+    SubscribeMessage,
+    WebSocketGateway,
+    OnGatewayConnection,
+    WebSocketServer,
+    MessageBody,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
-    path: '/chat',
+    namespace: 'chat',
     cors: {
         origin: '*',
     },
 })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection {
+    @WebSocketServer()
+    server: Server;
+
+    handleConnection(socket: Socket) {
+        console.log('client connected', socket.id);
+    }
+
     @SubscribeMessage('message')
-    handleMessage(client: any, payload: any): string {
-        console.log('client', client);
-        console.log('payload', payload);
-        return 'Hello world!';
+    handleMessage(@MessageBody() data: any) {
+        this.server.emit('received_message', '서버에서 응답보냄');
     }
 }
